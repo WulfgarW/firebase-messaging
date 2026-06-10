91d72f7c2b15b1c6f68d0f8268bc575d753f0015
Steven B. <51370195+sdb9696@users.noreply.github.com>
2026-06-10 12:57:55 +0100

    Use lazy logging and warn when listen wait for reset times out

--- firebase_messaging/fcmpushclient.py
+++ firebase_messaging/fcmpushclient.py
@@ -698,16 +698,31 @@ class FcmPushClient:  # pylint:disable=too-many-instance-attributes
                         ):
                             if (counter > 0) and (counter % 10 == 0):
                                 _logger.debug(
-                                    "Listen is waiting for reset to "
-                                    f"succeed. Already slept for {counter}s, and "
-                                    f"run state is still {self.run_state}."
+                                    "Listen is waiting for reset to succeed. "
+                                    "Already slept for %ss, and run state "
+                                    "is still %s.",
+                                    counter,
+                                    self.run_state,
                                 )
-                            counter = counter + 1
+                            counter += 1
                             await asyncio.sleep(1)
-                        _logger.info(
-                            f"In total listen waited {counter}s for reset "
-                            f"to succeed: run state is now {self.run_state}."
-                        )
+                        if self.run_state in (
+                            FcmPushClientRunState.RESETTING,
+                            FcmPushClientRunState.STARTING_CONNECTION,
+                        ):
+                            _logger.warning(
+                                "Listen gave up waiting for reset to succeed "
+                                "after %ss: run state is still %s.",
+                                counter,
+                                self.run_state,
+                            )
+                        else:
+                            _logger.debug(
+                                "Listen waited %ss for reset to succeed: "
+                                "run state is now %s.",
+                                counter,
+                                self.run_state,
+                            )
                     elif msg := await self._receive_msg():
                         await self._handle_message(msg)
 
